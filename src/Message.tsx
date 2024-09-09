0a91aa41f024839659f232740a88ee984c40ec21
chienchuanw <chienchuanwww@gmail.com>
2024-09-09 16:16:02 +0800

    Added condition statement in Message.tsx

--- src/Message.tsx
+++ src/Message.tsx
@@ -2,6 +2,9 @@
 // It's a convention to use Pascal Case in .tsx file
 function Message() {
   // The following code is JSX, which stands for JavaScript XML
+  const name = 'Frank';
+  if (name)
+    return <h1>Hello {name}</h1>;
   return <h1>Hello World</h1>;
 }
 
