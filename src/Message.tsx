37abf612ce4acb7408d50076e01ba27e1a12a85d
chienchuanw <chienchuanwww@gmail.com>
2024-09-09 16:02:54 +0800

    Created Message.jsx

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+
+// It's a convention to use Pascal Case in .tsx file
+function Message() {
+  // The following code is JSX, which stands for JavaScript XML
+  return <h1>Hello World</h1>;
+}
+
+// To use this Message function, we have to export it
+export default Message;
