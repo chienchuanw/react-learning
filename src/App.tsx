8d3b66943880c980b5765d91789168d58fb35b94
chienchuanw <chienchuanwww@gmail.com>
2024-09-12 11:35:50 +0800

    Finished Creating a ListGroup Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Message from './Message'
+import ListGroup from './components/ListGroup';
+
+// function App() {
+//   // We should always close our React component like the comment below. Otherwise, it will get a compilation error.
+//   // return <div><Message></Message></div>
+
+//   // We can also use self-closing syntax
+//   return <div><Message /></div>;
+// }
 
 function App() {
-  // We should always close our React component like the comment below. Otherwise, it will get a compilation error.
-  // return <div><Message></Message></div>
-
-  // We can also use self-closing syntax
-  return <div><Message /></div>;
+  return <div><ListGroup /></div>
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+// import './index.css'
+
+// We use Bootstrap for CSS styling instead of index.css
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
