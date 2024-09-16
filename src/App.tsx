bf757e897c7773e7b06464003fd62a04300f2b53
chienchuanw <chienchuanwww@gmail.com>
2024-09-16 11:41:28 +0800

    Doing Exercise: Building a Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import Message from './Message';
 import ListGroup from './components/ListGroup';
 import Alert from './components/Alert';
+import Button from './components/Button';
 
-// function App() {
-//   // We should always close our React component like the comment below. Otherwise, it will get a compilation error.
-//   // return <div><Message></Message></div>
+function AppMessage() {
+  // We should always close our React component like the comment below. Otherwise, it will get a compilation error.
+  // return <div><Message></Message></div>
 
-//   // We can also use self-closing syntax
-//   return <div><Message /></div>;
-// }
+  // We can also use self-closing syntax
+  return <div><Message /></div>;
+}
 
 function App() {
   let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
@@ -27,7 +28,16 @@ function AppAlert() {
       </Alert>
     </div>
   )
-
 }
 
-export default AppAlert;
+function AppButton() {
+  return (
+    <div>
+      <Button>
+        This is a button!
+      </Button>
+    </div>
+  )
+}
+
+export default AppButton;
---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {
+  children: string
+}
+
+const Button = ({ children }: Props) => {
+  return (
+    <button className='btn btn-primary'>{ children }</button>
+  )
+}
+
+export default Button
