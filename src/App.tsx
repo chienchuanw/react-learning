a02062e10dff3830b2e569e01c35a02baf688a97
chienchuanw <chienchuanwww@gmail.com>
2024-10-01 12:55:09 +0800

    Updating Nested Objects

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,28 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [drink, setDrink] = useState({
-    title: "Americano",
-    price: 5,
+  const [customer, setCustomer] = useState({
+    name: 'John',
+    address: {
+      city: 'San Francisco',
+      zipCode: 94111
+    }
   });
 
-  
+  // Update zipCode
   const handleClick = () => {
-    setDrink({ ...drink, price: 6});
+    setCustomer({ 
+      ...customer, // Notice that spread operator in JS can only perform a shallow copy. In other words, it will not copy the nested properties of an object but reference the same object in memory. That is not what we want because every time we modify the address, it will affect the others.
+      address: { ...customer.address, zipCode: 94112 } // That's why we need to use spread operator again to copy and thus update the zipCode.
+    })  
   }
 
   return (
     <div>
-      {drink.price}
-      <button onClick={handleClick}>Click Me</button>
+      {customer.name}
+      {customer.address.city}
+      {customer.address.zipCode}
+      <button onClick={handleClick}>Change Zip Code</button>
     </div>
   )
 
