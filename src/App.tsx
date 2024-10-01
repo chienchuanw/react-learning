27b6015f9619c29435d899d4a42e1746a2ceabd4
chienchuanw <chienchuanwww@gmail.com>
2024-10-01 12:45:08 +0800

    Updating Objects: refactor

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,9 @@ function App() {
     price: 5,
   });
 
-  // When click the button, it will update the price of the drink.
+  
   const handleClick = () => {
-    // Since we cannot directly update the state by assigning new value, we have to declare a new variable and pass it into setDrink.
-    const newDrink = {
-      // Since there might be a lot of properties in an object, it will be more efficient to use spread operator (...) to copy all the property at once and then update the target property.
-      ...drink,
-
-      // Update the target property
-      price: 6,
-    }
-    setDrink(newDrink); // Update the state.
+    setDrink({ ...drink, price: 6});
   }
 
   return (
