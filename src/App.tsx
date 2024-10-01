16b4079455984d501f78f24c4e5bda0759c6343f
chienchuanw <chienchuanwww@gmail.com>
2024-10-01 12:43:21 +0800

    Updating Objects: using spread operator to copy all properties and values of an objects

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,8 @@ function App() {
   const handleClick = () => {
     // Since we cannot directly update the state by assigning new value, we have to declare a new variable and pass it into setDrink.
     const newDrink = {
-      title: drink.title,  // This will copy the old value as the value of title.
+      // Since there might be a lot of properties in an object, it will be more efficient to use spread operator (...) to copy all the property at once and then update the target property.
+      ...drink,
 
       // Update the target property
       price: 6,
